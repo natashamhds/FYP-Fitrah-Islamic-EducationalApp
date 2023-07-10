cdf6b7b651aa203049c4f0b2590a6c87b85a03b9
Nur Natasha <133860829+natashamhds@users.noreply.github.com>
2023-07-10 22:55:53 +0800

    Update bar_item_page.dart

--- pages/Home/navpages/Marks Page/bar_item_page.dart
+++ pages/Home/navpages/Marks Page/bar_item_page.dart
@@ -1,9 +1,12 @@
 // LIHAT MARKAH SKRIN
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fitrah/config/configScheme.dart';
 import 'package:fitrah/widgets/app_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class BarItemPage extends StatefulWidget {
   const BarItemPage({super.key});
@@ -15,69 +18,97 @@ class BarItemPage extends StatefulWidget {
 class _BarItemPageState extends State<BarItemPage> {
   final currentUser = FirebaseAuth.instance.currentUser;
 
+  void successfullyDelete() {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: AwesomeSnackbarContent(
+        title: "Sejarah kuiz berjaya diapadam",
+        message: "Anda kini boleh cuba menjawab sekali lagi",
+        color: Colors.lightGreen,
+        contentType: ContentType.success,
+      ),
+      behavior: SnackBarBehavior.floating,
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    DocumentReference userDocRef =
-        FirebaseFirestore.instance.collection('users').doc(currentUser!.uid);
-    CollectionReference userRef = userDocRef.collection('anak');
-    
     return Scaffold(
         backgroundColor: lightBlue,
         appBar: AppBar(
           backgroundColor: Colors.transparent,
           elevation: 0,
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            children: [
-              StreamBuilder(
-        stream: FirebaseFirestore.instance
-                          .collection('users')
-                          .snapshots(),
-        builder: (context, AsyncSnapshot snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Text("Loading...");
-          }
-          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-            return Text('No data available');
-          }
-          
-          final subcollectionText = snapshot.data!.docs
-              .map((doc) => '${doc.data()}')
-              .join();
-
-          return Container(
-                  width: double.infinity,
-                  height: 80,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20), color: darkBlue),
-                  child: Center(
-                      child: AppText(
-                    text: subcollectionText,
-                    size: 30,
-                    color: Colors.white,
-                  )));
-        }),
-              
-              const SizedBox(height: 15),
-              AppText(
-                  text: "Sejarah Soalan Pengukuhan", size: 25, color: darkBlue),
-              Container(
-                margin: const EdgeInsets.only(top: 20, bottom: 20),
-                padding: const EdgeInsets.all(25),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.white),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    AppText(text: "Adab Belajar", size: 20),
-                    AppText(text: "100%", size: 20, color: darkBlue)
-                  ],
-                ),
-              )
-            ],
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              children: [
+                AppText(
+                    text: "Sejarah Soalan Pengukuhan",
+                    size: 25,
+                    color: darkBlue),
+                const SizedBox(height: 20),
+                StreamBuilder<QuerySnapshot>(
+                    stream: FirebaseFirestore.instance
+                        .collection('users')
+                        .doc(currentUser!.uid)
+                        .collection('anak')
+                        .snapshots(),
+                    builder: (context, AsyncSnapshot snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Center(
+                            child: CircularProgressIndicator(color: darkBlue));
+                      }
+                      if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                        return Center(
+                            child: AppText(text: "Uh-oh, Tiada maklumat!", size: 22));
+                      }
+                      return ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: snapshot.data?.docs.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            DocumentSnapshot documentSnapshot =
+                                snapshot.data!.docs[index];
+                            return Card(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20)),
+                                child: Slidable(
+                                    endActionPane: ActionPane(
+                                        motion: const DrawerMotion(),
+                                        children: [
+                                          SlidableAction(
+                                              backgroundColor: Colors.redAccent,
+                                              icon: Icons.delete_forever,
+                                              onPressed: (context) => {
+                                                    snapshot.data.docs[index]
+                                                        .reference
+                                                        .delete(),
+                                                    successfullyDelete()
+                                                  })
+                                        ]),
+                                    child: ListTile(
+                                      title: AppText(
+                                          text: documentSnapshot['anakName'],
+                                          size: 30),
+                                      subtitle: Text(
+                                          documentSnapshot['subjectName'],
+                                          style: const TextStyle(fontSize: 20)),
+                                      trailing: Wrap(
+                                        spacing: 12,
+                                        children: <Widget>[
+                                          AppText(
+                                              text:
+                                                  "${documentSnapshot['subjectScore']}%",
+                                              size: 30),
+                                        ],
+                                      ),
+                                    ))).animate().fade(duration: 800.ms).slideY(begin: 1.0, end: 0.0, curve: Curves.fastOutSlowIn);
+                          });
+                    }),
+              ],
+            ),
           ),
         ));
   }
