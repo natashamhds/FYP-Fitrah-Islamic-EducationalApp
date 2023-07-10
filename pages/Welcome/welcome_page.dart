9687644b98fbad4cc846e1b40aa71d1f56ee45f4
Nur Natasha <133860829+natashamhds@users.noreply.github.com>
2023-07-10 22:59:26 +0800

    Update welcome_page.dart

--- pages/Welcome/welcome_page.dart
+++ pages/Welcome/welcome_page.dart
@@ -15,83 +15,82 @@ class _WelcomePageState extends State<WelcomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: lightBlue,
-      body: SafeArea(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 30,
-            ),
-            Animate(
-              effects: [FadeEffect(duration: 1300.ms)],
-              child: Container(
-                width: MediaQuery.of(context)
-                    .size
-                    .width, //create container based on device's width
-                height: MediaQuery.of(context)
-                    .size
-                    .width*0.9,
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('asset/images/splash.png'),
-                        fit: BoxFit.contain)),
-              ),
-            ),
-            Flexible(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-              const Text(
-                "DIMANA KANAK-KANAK BELAJAR MENGENALI ISLAM",
-                style: TextStyle(fontSize: 12, fontFamily: 'circe'),
-              ),
-              const Text("Memupuk Rasa Cinta \n & \n Membina Iman",
-                  style: TextStyle(
-                      fontSize: 26,
-                      fontWeight: FontWeight.w700,
-                      fontFamily: 'circe'),
-                  textAlign: TextAlign.center),
-              const Text(
-                  "Akhlak-akhlak indah nabi, \n dan mengenali huruf Jawi \n hanya dengan beberapa klik sahaja",
-                  style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.w500,
-                      fontFamily: 'circe'),
-                  textAlign: TextAlign.center),
-              Center(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Animate(
+                effects: [FadeEffect(duration: 1300.ms)],
                 child: Container(
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                        width: 15,
-                        color: Colors.black.withOpacity(0.1),
-                      )),
+                  width: MediaQuery.of(context)
+                      .size
+                      .width, //create container based on device's width
+                  height: MediaQuery.of(context)
+                      .size
+                      .width*0.9,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/quran.png'),
+                          fit: BoxFit.contain)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 30, bottom: 30),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                const Text(
+                  "MARI BELAJAR MENGENALI ISLAM",
+                  style: TextStyle(fontSize: 12, fontFamily: 'circe'),
+                ),
+                const Text("Memupuk Rasa Cinta \n & \n Membina Iman",
+                    style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.w700,
+                        fontFamily: 'circe'),
+                    textAlign: TextAlign.center),
+                const Text(
+                    "Akhlak-akhlak indah nabi, \n dan mengenali huruf Jawi \n hanya dengan beberapa klik sahaja",
+                    style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500,
+                        fontFamily: 'circe'),
+                    textAlign: TextAlign.center),
+                Center(
                   child: Container(
-                    padding: const EdgeInsets.all(5),
                     decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: darkBlue,
-                    ),
-                    child: IconButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const AuthPage()));
-                      },
-                      icon: const Icon(
-                        Icons.arrow_forward_ios,
-                        color: Colors.white,
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          width: 15,
+                          color: Colors.black.withOpacity(0.1),
+                        )),
+                    child: Container(
+                      padding: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: darkBlue,
+                      ),
+                      child: IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const AuthPage()));
+                        },
+                        icon: const Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
+                )
+                  ],
                 ),
               )
-                ],
-              ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
-}
