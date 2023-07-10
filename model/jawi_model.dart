00174319ce8ccb0aab18aa6de8bba81d97be63ff
Nur Natasha <133860829+natashamhds@users.noreply.github.com>
2023-07-10 22:53:02 +0800

    Update jawi_model.dart

--- model/jawi_model.dart
+++ model/jawi_model.dart
@@ -1,127 +1,131 @@
 class JawiModel {
-  final String imagePath;
+  JawiModel(
+      {required this.imagePath,
+      required this.audioPath,
+      required this.huruf});
+
   final String audioPath;
   final String huruf;
-
-  JawiModel(
-      {required this.imagePath, required this.audioPath, required this.huruf});
+  final String imagePath;
 }
 
-List<JawiModel> imageList = [
-  JawiModel(
-      imagePath: 'asset/images/Alif.PNG',
-      audioPath: 'asset/audio/alif.mp3',
-      huruf: "Alif"),
-  JawiModel(
-      imagePath: 'asset/images/Ba.PNG',
-      audioPath: 'asset/audio/ba.mp3',
-      huruf: "Ba"),
-  JawiModel(
-      imagePath: 'asset/images/Ta.PNG',
-      audioPath: 'asset/audio/ta.mp3',
-      huruf: "Ta"),
-  JawiModel(
-      imagePath: 'asset/images/Sa.PNG',
-      audioPath: 'asset/audio/sa.mp3',
-      huruf: "Sa"),
-  JawiModel(
-      imagePath: 'asset/images/Jim.PNG',
-      audioPath: 'asset/audio/jim.mp3',
-      huruf: "Jim"),
-  JawiModel(
-      imagePath: 'asset/images/Ha.PNG',
-      audioPath: 'asset/audio/ha.mp3',
-      huruf: "Ha"),
-  JawiModel(
-      imagePath: 'asset/images/Kho.PNG',
-      audioPath: 'asset/audio/kho.mp3',
-      huruf: "Kho"),
-  JawiModel(
-      imagePath: 'asset/images/Dal.PNG',
-      audioPath: 'asset/audio/dal.mp3',
-      huruf: "Dal"),
-  JawiModel(
-      imagePath: 'asset/images/Dzal.PNG',
-      audioPath: 'asset/audio/dzal.mp3',
-      huruf: "Dzal"),
-  JawiModel(
-      imagePath: 'asset/images/Ra.PNG',
-      audioPath: 'asset/audio/ra.mp3',
-      huruf: "Ra"),
-  JawiModel(
-      imagePath: 'asset/images/Zai.PNG',
-      audioPath: 'asset/audio/zai.mp3',
-      huruf: "Zai"),
-  JawiModel(
-      imagePath: 'asset/images/Sin.PNG',
-      audioPath: 'asset/audio/sin.mp3',
-      huruf: "Sin"),
-  JawiModel(
-      imagePath: 'asset/images/Shin.PNG',
-      audioPath: 'asset/audio/shin.mp3',
-      huruf: "Shin"),
-  JawiModel(
-      imagePath: 'asset/images/Sod.PNG',
-      audioPath: 'asset/audio/sod.mp3',
-      huruf: "Sod"),
-  JawiModel(
-      imagePath: 'asset/images/Dhod.PNG',
-      audioPath: 'asset/audio/dhod.mp3',
-      huruf: "Dhod"),
-  JawiModel(
-      imagePath: 'asset/images/Tho.PNG',
-      audioPath: 'asset/audio/tho.mp3',
-      huruf: "Tho"),
-  JawiModel(
-      imagePath: 'asset/images/Dzo.PNG',
-      audioPath: 'asset/audio/dzo.mp3',
-      huruf: "Dzo"),
-  JawiModel(
-      imagePath: 'asset/images/Ain.PNG',
-      audioPath: 'asset/audio/ain.mp3',
-      huruf: "Ain"),
-  JawiModel(
-      imagePath: 'asset/images/Ghain.PNG',
-      audioPath: 'asset/audio/ghain.mp3',
-      huruf: "Ghain"),
-  JawiModel(
-      imagePath: 'asset/images/Fa.PNG',
-      audioPath: 'asset/audio/fa.mp3',
-      huruf: "Fa"),
-  JawiModel(
-      imagePath: 'asset/images/Qaf.PNG',
-      audioPath: 'asset/audio/qaf.mp3',
-      huruf: "Qaf"),
-  JawiModel(
-      imagePath: 'asset/images/Kaf.PNG',
-      audioPath: 'asset/audio/kaf.mp3',
-      huruf: "Kaf"),
-  JawiModel(
-      imagePath: 'asset/images/Lam.PNG',
-      audioPath: 'asset/audio/lam.mp3',
-      huruf: "Lam"),
-  JawiModel(
-      imagePath: 'asset/images/Mim.PNG',
-      audioPath: 'asset/audio/mim.mp3',
-      huruf: "Mim"),
-  JawiModel(
-      imagePath: 'asset/images/Nun.PNG',
-      audioPath: 'asset/audio/nun.mp3',
-      huruf: "Nun"),
-  JawiModel(
-      imagePath: 'asset/images/Wau.PNG',
-      audioPath: 'asset/audio/wau.mp3',
-      huruf: "Wau"),
-  JawiModel(
-      imagePath: 'asset/images/Haa.PNG',
-      audioPath: 'asset/audio/haa.mp3',
-      huruf: "Haa"),
-  JawiModel(
-      imagePath: 'asset/images/Hamzah.PNG',
-      audioPath: 'asset/audio/hamzah.mp3',
-      huruf: "Hamzah"),
-  JawiModel(
-      imagePath: 'asset/images/Ya.PNG',
-      audioPath: 'asset/audio/ya.mp3',
-      huruf: "Ya")
-];
+class JawiRecognizer {
+  List<JawiModel> alphabetList = [
+    JawiModel(
+        imagePath: 'asset/images/Alif.png',
+        audioPath: 'audio/Alif.mp3',
+        huruf: "Alif"),
+    JawiModel(
+        imagePath: 'asset/images/Ba.png',
+        audioPath: 'audio/Ba.mp3',
+        huruf: "Ba"),
+    JawiModel(
+        imagePath: 'asset/images/Ta.png',
+        audioPath: 'audio/Ta.mp3',
+        huruf: "Ta"),
+    JawiModel(
+        imagePath: 'asset/images/Sa.png',
+        audioPath: 'audio/Sa.mp3',
+        huruf: "Sa"),
+    JawiModel(
+        imagePath: 'asset/images/Jim.png',
+        audioPath: 'audio/Jim.mp3',
+        huruf: "Jim"),
+    JawiModel(
+        imagePath: 'asset/images/Ha.png',
+        audioPath: 'audio/Ha.mp3',
+        huruf: "Ha"),
+    JawiModel(
+        imagePath: 'asset/images/Kho.png',
+        audioPath: 'audio/Kho.mp3',
+        huruf: "Kho"),
+    JawiModel(
+        imagePath: 'asset/images/Dal.png',
+        audioPath: 'audio/Dal.mp3',
+        huruf: "Dal"),
+    JawiModel(
+        imagePath: 'asset/images/Dzal.png',
+        audioPath: 'audio/Dzal.mp3',
+        huruf: "Dzal"),
+    JawiModel(
+        imagePath: 'asset/images/Ra.png',
+        audioPath: 'audio/Ra.mp3',
+        huruf: "Ra"),
+    JawiModel(
+        imagePath: 'asset/images/Zai.png',
+        audioPath: 'audio/Zai.mp3',
+        huruf: "Zai"),
+    JawiModel(
+        imagePath: 'asset/images/Sin.png',
+        audioPath: 'audio/Sin.mp3',
+        huruf: "Sin"),
+    JawiModel(
+        imagePath: 'asset/images/Shin.png',
+        audioPath: 'audio/Shin.mp3',
+        huruf: "Shin"),
+    JawiModel(
+        imagePath: 'asset/images/Sod.PNG',
+        audioPath: 'audio/Sod.mp3',
+        huruf: "Sod"),
+    JawiModel(
+        imagePath: 'asset/images/Dhod.PNG',
+        audioPath: 'audio/Dhod.mp3',
+        huruf: "Dhod"),
+    JawiModel(
+        imagePath: 'asset/images/Tho.png',
+        audioPath: 'audio/Tho.mp3',
+        huruf: "Tho"),
+    JawiModel(
+        imagePath: 'asset/images/Dzo.PNG',
+        audioPath: 'audio/Dzo.mp3',
+        huruf: "Dzo"),
+    JawiModel(
+        imagePath: 'asset/images/Ain.png',
+        audioPath: 'audio/Ain.mp3',
+        huruf: "Ain"),
+    JawiModel(
+        imagePath: 'asset/images/Ghain.png',
+        audioPath: 'audio/Ghain.mp3',
+        huruf: "Ghain"),
+    JawiModel(
+        imagePath: 'asset/images/Fa.png',
+        audioPath: 'audio/Fa.mp3',
+        huruf: "Fa"),
+    JawiModel(
+        imagePath: 'asset/images/Qaf.png',
+        audioPath: 'audio/Qaf.mp3',
+        huruf: "Qaf"),
+    JawiModel(
+        imagePath: 'asset/images/Kaf.png',
+        audioPath: 'audio/Kaf.mp3',
+        huruf: "Kaf"),
+    JawiModel(
+        imagePath: 'asset/images/Lam.png',
+        audioPath: 'audio/Lam.mp3',
+        huruf: "Lam"),
+    JawiModel(
+        imagePath: 'asset/images/Mim.png',
+        audioPath: 'audio/Mim.mp3',
+        huruf: "Mim"),
+    JawiModel(
+        imagePath: 'asset/images/Nun.png',
+        audioPath: 'audio/Nun.mp3',
+        huruf: "Nun"),
+    JawiModel(
+        imagePath: 'asset/images/Wau.png',
+        audioPath: 'audio/Wau.mp3',
+        huruf: "Wau"),
+    JawiModel(
+        imagePath: 'asset/images/Haa.png',
+        audioPath: 'audio/Haa.mp3',
+        huruf: "Haa"),
+    JawiModel(
+        imagePath: 'asset/images/Hamzah.PNG',
+        audioPath: 'audio/Hamzah.mp3',
+        huruf: "Hamzah"),
+    JawiModel(
+        imagePath: 'asset/images/Ya.png',
+        audioPath: 'audio/Ya.mp3',
+        huruf: "Ya")
+  ];
+}
