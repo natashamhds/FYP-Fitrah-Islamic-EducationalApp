52c996cc99058ae619310077fda48e75ac87745c
Nur Natasha <133860829+natashamhds@users.noreply.github.com>
2023-07-10 22:54:57 +0800

    Update course_card.dart

--- pages/Home/navpages/Home Page/course_card.dart
+++ pages/Home/navpages/Home Page/course_card.dart
@@ -1,20 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:fitrah/config/configScheme.dart';
 import 'package:fitrah/pages/Course/course_akhlak.dart';
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class CourseCard extends StatelessWidget {
-  final DocumentSnapshot documentSnapshot;
-  final String title;
-  final String image;
-  final Function()? onTap;
-
   const CourseCard(
       {super.key,
+      this.animation,
       required this.documentSnapshot,
       required this.title,
       required this.image,
+      required this.bio,
+      required this.endColor,
       this.onTap});
+
+  final Function()? onTap;
+  final Animation<double>? animation; 
+  final String bio;
+  final DocumentSnapshot documentSnapshot;
+  final String image;
+  final String endColor;
+  final String title;
 
   @override
   Widget build(BuildContext context) {
@@ -27,36 +33,80 @@ class CourseCard extends StatelessWidget {
                   documentSnapshot: documentSnapshot, title: title)),
         )
       },
-      child: Container(
-        margin: const EdgeInsets.only(right: 15, top: 10),
-        width: 200,
-        height: 275,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            gradient: LinearGradient(
-                colors: [lightBlue, yellow],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight),
-            boxShadow: [
-              BoxShadow(
-                  color: lighterlightBlue,
-                  blurRadius: 12,
-                  offset: const Offset(0, 0.015))
-            ]
-            // color: Colors.white,
-            // image: DecorationImage(
-            //     image: AssetImage('asset/images/$image'), fit: BoxFit.cover)
-            ),
-        child: Padding(
-            padding: const EdgeInsetsDirectional.only(start: 10),
-            child: Text(title,
-                style: const TextStyle(
-                  fontFamily: 'circe',
-                  fontSize: 28,
-                  color: Colors.black54,
-                  fontWeight: FontWeight.w500
-                ))),
-      ),
+      child: Stack(children: [
+        Container(
+          margin: const EdgeInsets.only(right: 15, top: 10),
+          width: 200,
+          height: 275,
+          decoration: BoxDecoration(
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                    color: HexColor(endColor).withOpacity(0.6),
+                    blurRadius: 8.0,
+                    offset: const Offset(1.1, 4.0))
+              ],
+              borderRadius: const BorderRadius.only(
+                bottomRight: Radius.circular(8.0),
+                bottomLeft: Radius.circular(8.0),
+                topLeft: Radius.circular(8.0),
+                topRight: Radius.circular(54.0),
+              ),
+              gradient: LinearGradient(
+                  colors: [HexColor('#FE95B6'), HexColor(endColor)],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight)),
+          child: Padding(
+              padding: const EdgeInsets.only(
+                  top: 80, left: 16, right: 16, bottom: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(title,
+                      textAlign: TextAlign.start,
+                      style: const TextStyle(
+                          fontFamily: 'circe',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          letterSpacing: 0.2,
+                          color: Colors.white)),
+                  Expanded(
+                      child: Padding(
+                          padding: const EdgeInsets.only(top: 8, bottom: 8),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(bio,
+                                  style: const TextStyle(
+                                      fontFamily: 'circe',
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 18,
+                                      letterSpacing: 0.2,
+                                      color: Colors.white))
+                            ],
+                          )))
+                ],
+              )),
+        ),
+        Positioned(
+            top: 0,
+            left: 0,
+            child: Container(
+              width: 84,
+              height: 84,
+              decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.2), shape: BoxShape.circle),
+            )),
+        Positioned(
+            top: 0,
+            left: 0,
+            child: Container(
+              width: 88,
+              height: 80,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/$image'))),
+            ))
+      ]),
     );
   }
 }
