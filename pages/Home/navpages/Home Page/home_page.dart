cdd59272b757c032524f087101866a5740087dda
Nur Natasha <133860829+natashamhds@users.noreply.github.com>
2023-07-10 22:55:17 +0800

    Update home_page.dart

--- pages/Home/navpages/Home Page/home_page.dart
+++ pages/Home/navpages/Home Page/home_page.dart
@@ -5,6 +5,8 @@ import 'package:fitrah/config/configScheme.dart';
 import 'package:fitrah/pages/Home/navpages/Home%20Page/quiz_card.dart';
 import 'package:fitrah/widgets/app_large_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -36,9 +38,6 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
               child: AppLargeText(text: "Pembelajaran"),
             ),
 
-            const SizedBox(
-                height: 20), // create distance between up and container
-
             // tab bar
             Align(
               alignment: Alignment.centerLeft,
@@ -63,7 +62,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
             ),
             Container(
               padding: const EdgeInsets.only(left: 20),
-              height: 275,
+              height: 260,
               width: double.maxFinite,
               child: TabBarView(
                 controller: tabController,
@@ -76,52 +75,52 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                       builder: (context, AsyncSnapshot snapshot) {
                         if (snapshot.hasData) {
                           return ListView.builder(
-                              itemCount: snapshot.data?.docs.length,
-                              scrollDirection: Axis.horizontal,
-                              itemBuilder: (BuildContext context, int index) {
-                                DocumentSnapshot documentSnapshot =
-                                    snapshot.data.docs[index];
+                                  itemCount: snapshot.data?.docs.length,
+                                  scrollDirection: Axis.horizontal,
+                                  itemBuilder: (BuildContext context, int index) {
+                                    DocumentSnapshot documentSnapshot =
+                                        snapshot.data.docs[index];
 
-                                return CourseCard(
-                                  title: documentSnapshot['title'],
-                                  image: documentSnapshot['image'],
-                                  documentSnapshot: documentSnapshot,
-                                );
-                              });
+                                    return CourseCard(
+                                      title: documentSnapshot['title'],
+                                      image: documentSnapshot['image1'],
+                                      bio: documentSnapshot['bio']
+                                          .replaceAll('\\n', '\n'),
+                                      endColor: documentSnapshot['endColor'],
+                                      documentSnapshot: documentSnapshot,
+                                    );
+                                  })
+                              .animate()
+                              .fade(duration: 1300.ms)
+                              .slideX(
+                                  begin: 1.0,
+                                  end: 0.0,
+                                  curve: Curves.fastOutSlowIn);
                         } else {
                           return Center(
-                            child:
-                                CircularProgressIndicator(color: darkBlue),
+                            child: CircularProgressIndicator(color: darkBlue),
                           );
                         }
                       }),
                   // Pembelajaran Jawi
                   ListView.builder(
-                      itemCount: 1,
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (BuildContext context, int index) {
-                        return GestureDetector(
-                          onTap: () => {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const CourseJawi()),
-                            )
-                          },
-                          child: Container(
-                            margin: const EdgeInsets.only(right: 15, top: 10),
-                            width: 200,
-                            height: 275,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                color: Colors.white,
-                                image: const DecorationImage(
-                                    image: AssetImage(
-                                        "asset/images/female_quran.png"),
-                                    fit: BoxFit.cover)),
-                          ),
-                        );
-                      })
+                          itemCount: 1,
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (BuildContext context, int index) {
+                            return GestureDetector(
+                              onTap: () => {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const CourseJawi()),
+                                )
+                              },
+                              child: const JawiCard(),
+                            );
+                          })
+                      .animate()
+                      .fade(duration: 1300.ms)
+                      .slideX(begin: 1.0, end: 0.0, curve: Curves.fastOutSlowIn)
                 ],
               ),
             ),
@@ -164,10 +163,10 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                                 snapshot.data.docs[index];
 
                             return Quizcard(
-                              title: documentSnapshot['title'],
+                                title: documentSnapshot['title'],
                                 image: documentSnapshot['image'],
                                 documentSnapshot: documentSnapshot);
-                          });
+                          }).animate().fade(duration: 800.ms).slideX(begin: 1.0, end: 0.0, curve: Curves.fastOutSlowIn);
                     } else {
                       return Center(
                         child: CircularProgressIndicator(color: darkBlue),
@@ -182,12 +181,93 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   }
 }
 
+class JawiCard extends StatelessWidget {
+  const JawiCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      Container(
+        margin: const EdgeInsets.only(right: 15, top: 10),
+        width: 200,
+        height: 275,
+        decoration: BoxDecoration(
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                  color: HexColor('#FFB295').withOpacity(0.6),
+                  blurRadius: 8.0,
+                  offset: const Offset(1.1, 4.0))
+            ],
+            borderRadius: const BorderRadius.only(
+              bottomRight: Radius.circular(8.0),
+              bottomLeft: Radius.circular(8.0),
+              topLeft: Radius.circular(8.0),
+              topRight: Radius.circular(54.0),
+            ),
+            gradient: LinearGradient(
+                colors: [HexColor('#FE95B6'), HexColor('#FFB295')],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight)),
+        child: const Padding(
+            padding: EdgeInsets.only(top: 80, left: 16, right: 16, bottom: 8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Cara Menulis Jawi",
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                        fontFamily: 'circe',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        letterSpacing: 0.2,
+                        color: Colors.white)),
+                Expanded(
+                    child: Padding(
+                        padding: EdgeInsets.only(top: 8, bottom: 8),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                                "Belajar bagaimana \n cara menulis \n dan menyebut \n huruf Jawi",
+                                style: TextStyle(
+                                    fontFamily: 'circe',
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 18,
+                                    letterSpacing: 0.2,
+                                    color: Colors.white))
+                          ],
+                        )))
+              ],
+            )),
+      ),
+      Positioned(
+          top: 0,
+          left: 0,
+          child: Container(
+            width: 84,
+            height: 84,
+            decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.2), shape: BoxShape.circle),
+          )),
+      Positioned(
+          top: 0,
+          left: 0,
+          child: SizedBox(
+            child:
+                Image.asset('assets/images/pencil.png', width: 88, height: 80),
+          ))
+    ]);
+  }
+}
+
 // ignore: must_be_immutable
 class CircleTabIndicator extends Decoration {
+  CircleTabIndicator({required this.color, required this.radius});
+
   final Color color;
   double radius;
-
-  CircleTabIndicator({required this.color, required this.radius});
 
   @override
   BoxPainter createBoxPainter([VoidCallback? onChanged]) {
@@ -196,10 +276,10 @@ class CircleTabIndicator extends Decoration {
 }
 
 class _CirclePainter extends BoxPainter {
+  _CirclePainter({required this.color, required this.radius});
+
   final Color color;
   double radius;
-
-  _CirclePainter({required this.color, required this.radius});
 
   @override
   void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
